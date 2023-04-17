e209da48172d971822454e7e50033241e7f71775
Totalinfonet <totalinfonet@gmail.com>
2023-04-17 20:25:50 +0300

    handleDeleteContact Replaced

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,95 +1,17 @@
-// import React from 'react';
-// import { nanoid } from '@reduxjs/toolkit';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { addContact, deleteContact } from '../../redux/contacts';
-// import { setFilter } from '../../redux/filter';
-// import { ContactForm } from '../ContactForm/ContactForm';
-// import { Filter } from '../Filter/Filter';
-// import { ContactList } from '../ContactList/ContactList';
-// import { AppWrapper, Title } from './App.styled';
-// import { useMemo } from 'react';
-
-// export const App = () => {
-//   const dispatch = useDispatch();
-//   const contacts = useSelector(state => state.contacts);
-//   const filter = useSelector(state => state.filter);
-
-//   const handleAddContact = (name, number) => {
-//     const existingContact = contacts.find(
-//       contact => contact.name.toLowerCase() === name.toLowerCase()
-//     );
-//     if (existingContact) {
-//       alert(`${name} is already in contacts.`);
-//       return;
-//     }
-//     const id = nanoid();
-//     dispatch(addContact({ id, name, number }));
-//   };
-
-//   const handleDeleteContact = id => {
-//     dispatch(deleteContact(id));
-//   };
-
-//   const handleFilterChange = e => {
-//     const { value } = e.target;
-//     dispatch(setFilter(value));
-//   };
-
-//   const filteredContacts = useMemo(() => {
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(filter.toLowerCase())
-//     );
-//   }, [contacts, filter]);
-
-//   return (
-//     <AppWrapper>
-//       <Title>Phonebook</Title>
-//       <ContactForm onSubmit={handleAddContact} />
-//       <Title>Contacts</Title>
-//       <Filter filter={filter} onChange={handleFilterChange} />
-//       <ContactList
-//         contacts={filteredContacts}
-//         onDeleteContact={handleDeleteContact}
-//       />
-//     </AppWrapper>
-//   );
-// };
-
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteContact } from '../../redux/contacts';
-// import { setFilter } from '../../redux/filter';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { Filter } from '../Filter/Filter';
 import { ContactList } from '../ContactList/ContactList';
 import { AppWrapper, Title } from './App.styled';
-import { useMemo } from 'react';
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(state => state.contacts);
-  const filter = useSelector(state => state.filter);
-
-  const handleDeleteContact = id => {
-    dispatch(deleteContact(id));
-  };
-
-  const filteredContacts = useMemo(() => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  }, [contacts, filter]);
-
   return (
     <AppWrapper>
       <Title>Phonebook</Title>
       <ContactForm />
       <Title>Contacts</Title>
       <Filter />
-      <ContactList
-        contacts={filteredContacts}
-        onDeleteContact={handleDeleteContact}
-      />
+      <ContactList />
     </AppWrapper>
   );
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,29 +1,34 @@
-import PropTypes from 'prop-types';
+import { useMemo } from 'react';
 import { ContactItem } from '../ContactItem/ContactItem';
 import { List } from './ContactList.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from '../../redux/contacts';
 
-export const ContactList = ({ contacts, onDeleteContact }) => {
+export const ContactList = () => {
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
+
+  const filteredContacts = useMemo(() => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }, [contacts, filter]);
+
+  const handleDeleteContact = id => {
+    dispatch(deleteContact(id));
+  };
+
   return (
     <List>
-      {contacts.map(contact => (
+      {filteredContacts.map(contact => (
         <ContactItem
           key={contact.id}
           name={contact.name}
           number={contact.number}
-          onDelete={() => onDeleteContact(contact.id)}
+          onDelete={() => handleDeleteContact(contact.id)}
         />
       ))}
     </List>
   );
-};
-
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
 };
